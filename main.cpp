8111e188262d1a576fd90833ba07d92cc9e1ebfd
higarbagexd <mustafamehboobsiddiqui@gmail.com>
2026-08-11 21:45:06 +0300

    Added copyright notice comment

--- main.cpp
+++ main.cpp
@@ -1,3 +1,25 @@
+/*
+ * 2D-Input-Display
+ *
+ * Copyright (C) 2026 higarbagexd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
